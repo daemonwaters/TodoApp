6b07e0be1737b74dedc179ce3848876e0502a337
Sir Ali <sirali@Sirs-MacBook-Pro.local>
2021-12-09 16:40:47 +0330

    additional changes

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,45 +1,58 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-function Input({inputText,setInputText,todos,setTodos}) {
+function Input({ inputText, setInputText, todos, setTodos }) {
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
 
-    const handleChange = (e)=>{
-        setInputText(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputText === "") {
+      alert("please add a task");
+      return;
     }
 
-    const handleSubmit = (e)=>{
-        e.preventDefault();
+    const exist = todos.some(
+      (el) => el.toLowerCase() === inputText.toLowerCase()
+    );
 
-        if(inputText===''){
-            alert('please add a task')
-            return
-        }
-
-        const exist = todos.some(el=>el.toLowerCase()===inputText.toLowerCase())
-
-        if(exist){
-            alert('The task you are trying to add already exists');
-            setInputText('');
-            return
-        }
-        
-
-        setTodos([
-            ...todos,inputText
-        ])
-
-
-        setInputText('');
+    if (exist) {
+      alert("The task you are trying to add already exists");
+      setInputText("");
+      return;
     }
 
+    setTodos([...todos, inputText]);
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input value={inputText} onChange={handleChange} type="text" name="todo" placeholder="Plans..."/>
-            <button type="submit">
-                Add
-            </button>
-        </form>
-    )
+    setInputText("");
+  };
+
+  useEffect(() => {
+    let data = localStorage.getItem("todos");
+    if (data) {
+      setTodos(JSON.parse(data));
+    }
+    //runs on initial render and if there's data in local storage , set's our state to that data
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+    //this effect runs anytime our todos state changes and adds the todo to the local storage
+  }, [todos]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={inputText}
+        onChange={handleChange}
+        type="text"
+        name="todo"
+        placeholder="Plans..."
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
 }
 
-export default Input
+export default Input;
